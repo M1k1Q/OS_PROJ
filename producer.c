0182dee1f417547b016c5db5ea59d768ff33a7c6
M1k1Q <164217715+M1k1Q@users.noreply.github.com>
2025-07-27 04:26:20 +0500

    Update producer.c
    Added , error checks if Exterior or Interior stack lists are full

--- producer.c
+++ producer.c
@@ -1,6 +1,14 @@
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "factory.h"
@@ -13,22 +21,36 @@ void *Prod(void *arg) {
     int paused = isPaused;
     pthread_mutex_unlock(&pauseMutex);
 
-    if (paused == 1) {
-      // sleep(1);
-      printf("Producer paused\n");
+    if (paused) {
+      printf("Producer %d paused\n", dat->id);
+      sleep(1);
       continue;
     }
 
     int part_ID = rand() % 100;
-    int part_t = rand() % 2;
+    part_Type part_t = rand() % 2;
     sleep(2);
     if (isShutDown) break;
 
-    if (consumed_list.top == MAX_PARTS) {
-      printf("consumer list full");
+    if (part_t == Interior) {
+      pthread_mutex_lock(&Int_list.mutex);
+      if (Int_list.top >= MAX_PARTS) {
+        pthread_mutex_unlock(&Int_list.mutex);
+        printf("Producer %d waiting: Interior list full\n", dat->id);
+        continue;
+      }
+      pthread_mutex_unlock(&Int_list.mutex);
+    } else {
+      pthread_mutex_lock(&Ext_list.mutex);
+      if (Ext_list.top >= MAX_PARTS) {
+        pthread_mutex_unlock(&Ext_list.mutex);
+        printf("Producer %d waiting: Exterior list full\n", dat->id);
+        continue;
+      }
+      pthread_mutex_unlock(&Ext_list.mutex);
     }
 
-    if (sem_trywait(&consumed_list.empty) != 0) {
+    if (sem_trywait(&produced_list.empty) != 0) {
       if (isShutDown) break;
       continue;
     }
@@ -39,16 +61,18 @@ void *Prod(void *arg) {
       sem_post(&produced_list.empty);
       continue;
     }
+
+    // === PRODUCE PART ===
     Car_Part part = {.id = part_ID, .type = part_t};
     produced_list.stacc[produced_list.top++] = part;
-    // printf("produced_list top = %d\n", produced_list.top);
     pthread_mutex_unlock(&produced_list.mutex);
     sem_post(&produced_list.full);
 
+    // === LOGGING ===
     if (prod_log && Prod_log_ready && Prod_log_written) {
       sem_wait(Prod_log_written);
-      snprintf(prod_log->message, LOG_MSG_SIZE,
-               "Producer %d: ID=%d,Type = %s\n ", dat->id, part.id,
+      snprintf(prod_log->message, LOG_MSG_SIZE, "Producer %d: ID=%d, Type=%s\n",
+               dat->id, part.id,
                part.type == Interior ? "Interior" : "Exterior");
       sem_post(Prod_log_ready);
     }
