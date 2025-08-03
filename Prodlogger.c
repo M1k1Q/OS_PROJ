7b3107489e11b72f63015b1e4bf668071da21ee5
M1k1Q <164217715+M1k1Q@users.noreply.github.com>
2025-08-04 01:11:58 +0500

    Update Prodlogger.c
    Graceful shutdown implemented

--- Prodlogger.c
+++ Prodlogger.c
@@ -8,20 +8,18 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include "factory.h"  
+#include "factory.h"
 
 #define LOG_READY "/prod_sem_log_ready"
 #define LOG_WRITTEN "/prod_sem_log_written"
 #define LOG_SHM "/prod_log_shm"
 
 int main() {
-
   int shm_fd = shm_open(LOG_SHM, O_RDWR, 0666);
   if (shm_fd == -1) {
     perror("shm_open failed");
     exit(EXIT_FAILURE);
   }
-
 
   SharedLog *log = mmap(NULL, sizeof(SharedLog), PROT_READ | PROT_WRITE,
                         MAP_SHARED, shm_fd, 0);
@@ -29,7 +27,6 @@ int main() {
     perror("mmap failed");
     exit(EXIT_FAILURE);
   }
-
 
   sem_t *log_ready = sem_open(LOG_READY, 0);
   sem_t *log_written = sem_open(LOG_WRITTEN, 0);
@@ -39,7 +36,13 @@ int main() {
   }
 
   while (1) {
+    printf("Shutdown status : %d\n", log->shutdown);
     sem_wait(log_ready);
+    if (log->shutdown) {
+      printf("Exiting Prod logger\n");
+      sem_post(log_written);
+      break;
+    }
     printf("logger : %s\n", log->message);
     sem_post(log_written);
   }
